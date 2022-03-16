77d72fd27120657cc8a3ed71014550e26aed8f11
renatogm24 <renato.garay@pucp.pe>
2022-03-15 20:22:30 -0500

    react core

---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import "./list.css";
+
+function List() {
+  return (
+    <ul className="listBx">
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </ul>
+  );
+}
+
+export default List;
